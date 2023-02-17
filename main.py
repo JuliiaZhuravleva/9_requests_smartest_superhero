5e49edfeedbd6dd9030606a77ee6ea907ba9a940
Julia Zhuravleva <zhuravlevajuliia@gmail.com>
2023-02-17 13:20:55 +0400

    Уменьшено количество функций благодаря списковым включениям. Добавлен README с текстом задания

--- main.py
+++ main.py
@@ -8,11 +8,6 @@ def get_all_heroes():
     return json
 
 
-def get_hero_by_name(name):
-    required_hero = next(item for item in all_heroes if item['name'] == name)
-    return required_hero
-
-
 def get_smartest_hero_name(list_of_heroes):
     smartest_hero_name = None
     smartest_hero_int = 0
@@ -22,16 +17,14 @@ def get_smartest_hero_name(list_of_heroes):
             h_name = h['name']
             smartest_hero_name = h_name
             smartest_hero_int = h_int
-    return  smartest_hero_name
+    return smartest_hero_name
 
 
 superheroes = ['Hulk', 'Captain America', 'Thanos']
 base_url = 'https://akabab.github.io/superhero-api/api'
 all_heroes = get_all_heroes()
 
-compared_heroes = []
-for hero in superheroes:
-    compared_heroes.append(get_hero_by_name(hero))
+compared_heroes = [hero for hero in all_heroes if hero['name'] in superheroes]
 
 smartest_hero = get_smartest_hero_name(compared_heroes)
 
